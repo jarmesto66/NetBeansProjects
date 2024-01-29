4311a7f08e4f47d77928f20d84b79e001466674d
Javier Armesto <armesto.javier@gmail.com>
2024-01-29 18:22:38 +0100

    Casa Lunes primero

--- UD6_EstDinamicas/src/ud6_estdinamicas/Ej_87.java
+++ UD6_EstDinamicas/src/ud6_estdinamicas/Ej_87.java
@@ -16,16 +16,30 @@ public class Ej_87 {
     public static void main(String[] args) {
         
         ArrayList<Integer> valores = new ArrayList(); //Declaramos el ArrayList
-        int totalValores;
+        int totalValores, suma = 0, media = 0, max = 0, min = 100;
         totalValores = (int)(Math.random()*(20-10+1)+10); //Determinamos un número aleatorio de valores entre 10 y 20
         System.out.println("Van a ser "+totalValores+" valores");
         for (int i = 0; i < totalValores; i++) {
             valores.add((int)(Math.random()*(100-0+1)+0)); //Rellenamos con valores aleatorios de 0 a 100
         }
+        
         for (int i = 0; i < valores.size(); i++) {
-            System.out.println((i+1)+": "+valores.get(i)); //Imprimimos
+            suma += valores.get(i);
+            if (valores.get(i) < min) {
+                min = valores.get(i);
+            }
+            if (valores.get(i)>max) {
+                max = valores.get(i);
+            }
+        media = suma/totalValores;
             
         }
+        System.out.println(valores.toString()); //Imprimimos valores
+        System.out.println("Mínimo: "+ min);
+        System.out.println("Máximo: "+ max);
+        System.out.println("Suma: "+ suma);
+        System.out.println("Media: "+ media);
+                
     }
     
 }
