989955078d0f3d0a753fc49a34060b7794a9001a
Javier Armesto <armesto.javier@gmail.com>
2024-02-23 21:09:21 +0100

    eNTREGABLE

---
+++ Entregable_UD7_Blibi-OTK/src/entregable_ud7_blibi/otk/Libro.java
@@ -0,0 +1,24 @@
+
+package entregable_ud7_blibi.otk;
+
+/**
+ *
+ * @author JAVIER ARMESTO - 1º DAW
+ */
+public class Libro {
+    
+    //ATRIBUTOS DE LIBRO
+    private String titulo;
+    private String autor;
+    private enum genero {
+        FANTASIA,
+        DRAMA,
+        HISTORICO,
+        COMEDIA,
+        CIENCIA_FICCION,
+        EDUCATIVO,
+        OTROS
+    }
+    private int numCopias;
+    
+}
---
+++ Entregable_UD7_Blibi-OTK/src/entregable_ud7_blibi/otk/Principal.java
@@ -0,0 +1,17 @@
+
+package entregable_ud7_blibi.otk;
+
+/**
+ *
+ * @author JAVIER ARMESTO - 1º DAW
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
