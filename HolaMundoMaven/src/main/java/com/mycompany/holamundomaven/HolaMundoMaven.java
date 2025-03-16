0c2b3f2c667f97c8d4cda37a3b83503c33f59195
Javier Armesto <armesto.javier@gmail.com>
2025-03-16 13:33:41 +0100

    Con maven

---
+++ HolaMundoMaven/src/main/java/com/mycompany/holamundomaven/HolaMundoMaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundomaven;
+
+/**
+ *
+ * @author María
+ */
+public class HolaMundoMaven {
+
+    public static void main(String[] args) {
+        System.out.println("¡Hola Mundo!");
+    }
+}
